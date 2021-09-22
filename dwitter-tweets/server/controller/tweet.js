8ca6d808d85a556e8896bd04c5be47e6620a718c
smshack <5432tat@naver.com>
2021-09-22 02:50:05 +0000

    드위터 프로젝트 컨트롤러 생성

--- dwitter-tweets/server/controller/tweet.js
+++ dwitter-tweets/server/controller/tweet.js
@@ -1,5 +1,86 @@
 const {tweetRepository} = require('../data/tweet')
 
 let tweetController ={}
+tweetController.getTweets=(req,res)=>{
+    const username = req.query.username;
+    const data = username ? tweetRepository.getAllByUsername(username)
+                            :tweetRepository.getAll();
+    return res.status(200).json(data)
+}
 
+tweetController.getById=(req,res)=>{
+    const id = req.params.id;
+    const tweet = tweetRepository.getById(id)
+    if(tweet){
+        return res.status(200).json(tweet)
+    }else{
+        return res.status(404).json({message:"tweet id not found"})
+    }
+} 
+
+tweetController.create=(req,res)=>{
+    const {text,name,username} = req.body;
+  
+    const tweet = tweetRepository.create(text,name,username)
+    return res.status(201).json(tweet)
+} 
+
+tweetController.update=(req,res)=>{
+    const id = req.params.id;
+    const text = req.body.text;
+    const tweet = tweetRepository.update(id,text)
+    if(tweet){
+        return res.status(200).json(tweet)
+    }else{
+        return res.status(404).json({message:"tweet id not found"})
+    }
+}
+
+tweetController.delete=(req,res)=>{
+    const id = req.params.id;
+    tweetRepository.remove(id)
+    return res.sendStatus(204)
+}
+// router.get('/',(req,res,next)=>{
+//     const username = req.query.username;
+//     const data = username ? tweetRepository.getAllByUsername(username)
+//                             :tweetRepository.getAll();
+//     return res.status(200).json(data)
+// })
+
+// Get /tweets/:id
+// router.get('/:id',(req,res,next)=>{
+//     const id = req.params.id;
+//     const tweet = tweetRepository.getById(id)
+//     if(tweet){
+//         return res.status(200).json(tweet)
+//     }else{
+//         return res.status(404).json({message:"tweet id not found"})
+//     }
+// })
+// POST /tweets
+// router.post('/',(req,res,next)=>{
+    
+//     const {text,name,username} = req.body;
+  
+//     const tweet = tweetRepository.create(text,name,username)
+//     return res.status(201).json(tweet)
+// })
+// PUT /tweets/:id
+// router.put('/:id',(req,res,next)=>{
+//     const id = req.params.id;
+//     const text = req.body.text;
+//     const tweet = tweetRepository.update(id,text)
+//     if(tweet){
+//         return res.status(200).json(tweet)
+//     }else{
+//         return res.status(404).json({message:"tweet id not found"})
+//     }
+// })
+// DELETE /tweets/:id
+// router.delete('/:id',(req,res,next)=>{
+//     const id = req.params.id;
+//     tweetRepository.remove(id)
+//     return res.sendStatus(204)
+// })
 module.exports.tweetController=tweetController
--- dwitter-tweets/server/router/tweets.js
+++ dwitter-tweets/server/router/tweets.js
@@ -5,49 +5,16 @@ const {tweetController} = require('../controller/tweet')
 
 // GET /tweets
 // Get /tweets?username="username"
-router.get('/',(req,res,next)=>{
-    const username = req.query.username;
-    const data = username ? tweetRepository.getAllByUsername(username)
-                            :tweetRepository.getAll();
-    return res.status(200).json(data)
-
-})
+router.get('/',tweetController.getTweets)
 
 // Get /tweets/:id
-router.get('/:id',(req,res,next)=>{
-    const id = req.params.id;
-    const tweet = tweetRepository.getById(id)
-    if(tweet){
-        return res.status(200).json(tweet)
-    }else{
-        return res.status(404).json({message:"tweet id not found"})
-    }
-})
+router.get('/:id',tweetController.getById)
 // POST /tweets
-router.post('/',(req,res,next)=>{
-    
-    const {text,name,username} = req.body;
-  
-    const tweet = tweetRepository.create(text,name,username)
-    return res.status(201).json(tweet)
-})
+router.post('/',tweetController.create)
 // PUT /tweets/:id
-router.put('/:id',(req,res,next)=>{
-    const id = req.params.id;
-    const text = req.body.text;
-    const tweet = tweetRepository.update(id,text)
-    if(tweet){
-        return res.status(200).json(tweet)
-    }else{
-        return res.status(404).json({message:"tweet id not found"})
-    }
-})
+router.put('/:id',tweetController.update)
 // DELETE /tweets/:id
-router.delete('/:id',(req,res,next)=>{
-    const id = req.params.id;
-    tweetRepository.remove(id)
-    return res.sendStatus(204)
-})
+router.delete('/:id',tweetController.delete)
 
 module.exports = router;
 
